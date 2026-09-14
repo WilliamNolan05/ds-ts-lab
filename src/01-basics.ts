82a6b88fac8d1f5252d495b4697b75677665728b
williamarthurnolan@gmail.com <williamarthurnolan@gmail.com>
2026-09-14 12:13:56 +0100

    Complete type-safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -12,6 +12,11 @@ interface Colleague {
     contact: object; 
 }
 
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+}
+
 const friend1 = {
   name: "Paul Fleming",
   phone: "087-12345",
@@ -54,9 +59,11 @@ const colleague3 = {
     extension: 125,
   },
 };
-const colleagues = {
+
+export const colleagues : ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
+
 
 console.log(colleagues.current[0]);
